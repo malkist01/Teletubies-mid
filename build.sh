727a28aa52f3239215f222dd87aff0cf1c7a11e6
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-21 17:49:26 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ cp susfs4ksu/kernel_patches/fs/* ./fs
 cp susfs4ksu/kernel_patches/include/linux/* ./include/linux
 patch -p1 < 50_add_susfs_in_kernel-4.9.patch
 # Add KernelSU
-curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-main
+curl -LSs "https://raw.githubusercontent.com/WildKernels/Wild_KSU/wild/kernel/setup.sh" | bash -s wild
 #add KSU Config
 echo "Adding CONFIG_KSU.."
 echo "CONFIG_KSU=y" >> ./arch/arm64/configs/mido_defconfig
